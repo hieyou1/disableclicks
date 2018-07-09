b5877c664abc984f288ab607cdbb3ce4c3c45c94
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2018-07-09 08:08:11 -0500

    Inline style fixed
    Brackets suck dude

--- disableclicks.js
+++ disableclicks.js
@@ -1,12 +1,12 @@
 function disableClicks(olid) {
     var stuff = document.createElement("div");
     if (olid) {
-      stuff.setAttribute("style", "{bottom:0;left:0;position:fixed;right:0;top:0}");
+      stuff.setAttribute("style", "bottom:0;left:0;position:fixed;right:0;top:0");
       stuff.setAttribute("id", olid);
       document.body.appendChild(stuff);
       localStorage.setItem("olid", olid);
     } else {
-      stuff.setAttribute("style", "{bottom:0;left:0;position:fixed;right:0;top:0}");
+      stuff.setAttribute("style", "bottom:0;left:0;position:fixed;right:0;top:0");
       stuff.setAttribute("id", "takingClicks");
       document.body.appendChild(stuff);
       localStorage.setItem("olid", "takingClicks");
