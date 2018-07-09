c30e13f147ef7e6706f4ea4b8bc7fdc7ebe5f359
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2018-07-09 14:20:11 -0500

    Add an option for no localStorage

--- disableclicks.js
+++ disableclicks.js
@@ -1,23 +1,30 @@
-function disableClicks(olid) {
+function disableClicks(olid, ifls) {
     var stuff = document.createElement("div");
     if (olid) {
       stuff.setAttribute("style", "bottom:0;left:0;position:fixed;right:0;top:0");
       stuff.setAttribute("id", olid);
       document.body.appendChild(stuff);
-      localStorage.setItem("olid", olid);
+      if (ifls == false) {
+        localStorage.setItem("olid", olid);
+      }
     } else {
       stuff.setAttribute("style", "bottom:0;left:0;position:fixed;right:0;top:0");
       stuff.setAttribute("id", "takingClicks");
       document.body.appendChild(stuff);
-      localStorage.setItem("olid", "takingClicks");
+      if (ifls == false) {
+        localStorage.setItem("olid", "takingClicks");
+      }
     }
   }
-  function enableClicks() {
-    var olid = localStorage.getItem("olid");
+  function enableClicks(olid) {
+    if (!olid) {
+      var olid = localStorage.getItem("olid");
+    }
     if (!olid) {
       return false;
     } else {
       var olidelem = document.getElementById(olid);
       document.body.removeChild(olidelem);
+      localStorage.removeItem("olid");
     }
   }
